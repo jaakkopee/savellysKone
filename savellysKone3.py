25d3438d8999c5b53149aa445a3da4a27e2c6376
Jaakko Prättälä <iipekaj@gmail.com>
2023-10-10 05:52:23 +0300

    implemented ListGenerator class

--- savellysKone3.py
+++ savellysKone3.py
@@ -58,86 +58,27 @@ def generate(grammar, symbol, depth):
         return symbol
     else:
         return generate(grammar, generate_from_string(grammar, symbol), depth - 1)
+    
 
-pitch_grammar_str = """
-S -> A 
-A -> 60 A | 60 | 62 A | 62 | 60 B | 62 B
-B -> 64 B | 64 | 65 B | 65 | 64 C | 65 C
-C -> 67 C | 67 | 69 C | 69 | 67 D | 69 D
-D -> 72 D | 72 | 71 D | 71 | 69 E | 71 E
-E -> 67 E | 67 | 69 E | 69 | 67 F | 69 F
-F -> 65 F | 65 | 64 F | 64 | 62 G | 64 G
-G -> 60 G | 60 | 62 G | 62 | 60 H | 62 H
-H -> 60 H | 60 | 62 H | 62 | 60 A | 62 A
-"""
-pitch_grammar = parse_grammar(pitch_grammar_str.split("\n"))
-print(pitch_grammar)
-pitch_list = []
-print("generating pitch list")
-while len(pitch_list) < 8:
-    pitch_list = generate(pitch_grammar, "S", 64)
-    pitch_list = pitch_list.split()
-    pitch_list = [int(note) for note in pitch_list]
+class ListGenerator:
+    def __init__(self, grammar_str, min_length=8, type="pitch"):
+        self.type = type
+        self.grammar = parse_grammar(grammar_str.split("\n"))
+        self.min_length = min_length
+        self.list = []
 
-print("...done")
-
-
-duration_grammar_str = """
-S -> A B C D E F G H
-A -> 1 A | 0.5 A | 0.25 | 0.125
-B -> 1 B | 0.5 B | 0.25 | 0.125
-C -> 1 C | 0.5 C | 0.25 | 0.125
-D -> 1 D | 0.5 D | 0.25 | 0.125
-E -> 1 E | 0.5 E | 0.25 | 0.125
-F -> 1 F | 0.5 F | 0.25 | 0.125
-G -> 1 G | 0.5 G | 0.25 | 0.125
-H -> 1 H | 0.5 H | 0.25 | 0.125
-"""
-duration_grammar = parse_grammar(duration_grammar_str.split("\n"))
-print(duration_grammar)
-print("generating duration list")
-duration_list = []
-while len(duration_list) < 8:
-    duration_list = generate(duration_grammar, "S", 64)
-    duration_list = duration_list.split()
-    duration_list = [float(note) for note in duration_list]
-
-print("...done")
-
-velocity_grammar_str = """
-S -> A B C D E F G H
-A -> 100 A | 90 A | 80 | 70
-B -> 100 B | 90 B | 80 | 70
-C -> 100 C | 90 C | 80 | 70
-D -> 100 D | 90 D | 80 | 70
-E -> 100 E | 90 E | 80 | 70
-F -> 100 F | 90 F | 80 | 70
-G -> 100 G | 90 G | 80 | 70
-H -> 100 H | 90 H | 80 | 70
-"""
-velocity_grammar = parse_grammar(velocity_grammar_str.split("\n"))
-print(velocity_grammar)
-velocity_list = []
-print("generating velocity list")
-while len(velocity_list) < 8:
-    velocity_list = generate(velocity_grammar, "S", 64)
-    velocity_list = velocity_list.split()
-    velocity_list = [int(note) for note in velocity_list]
-
-print("...done")
-
-
-#find the shortest list
-shortest_list = len(pitch_list)
-if len(duration_list) < shortest_list:
-    shortest_list = len(duration_list)
-if len(velocity_list) < shortest_list:
-    shortest_list = len(velocity_list)
-
-#trim the lists to the shortest length
-pitch_list = pitch_list[:shortest_list]
-duration_list = duration_list[:shortest_list]
-velocity_list = velocity_list[:shortest_list]
+    def generate_list(self):
+        self.list = []
+        while len(self.list) < self.min_length:
+            self.list = generate(self.grammar, "S", 64)
+            self.list = self.list.split()
+            if self.type == "pitch":
+                self.list = [int(note) for note in self.list]
+            elif self.type == "duration": 
+                self.list = [float(note) for note in self.list]
+            elif self.type == "velocity":
+                self.list = [int(note) for note in self.list]
+        return self.list
 
 class Note:
     def __init__(self):
@@ -148,7 +89,7 @@ class Note:
         return    
     
 class Bar:
-    def __init__(self, onset=0, ioi=0.75):
+    def __init__(self, onset=0, ioi=0.75, pitch_list=None, duration_list=None, velocity_list=None):
         self.pitch_list = pitch_list
         self.duration_list = duration_list
         self.velocity_list = velocity_list
@@ -220,19 +161,52 @@ class Bar:
     
     
 class Song:
-    def __init__(self, num_bars=4):
+    def __init__(self, num_bars=4, pitch_generator=None, duration_generator=None, velocity_generator=None, generate_every_bar=False):
         self.bar_list = []
         self.ioi = 1.0
         self.num_bars = num_bars
+        self.pitch_generator = pitch_generator
+        self.duration_generator = duration_generator
+        self.velocity_generator = velocity_generator
+        self.pitch_list = []
+        self.duration_list = []
+        self.velocity_list = []
+        self.generate_parameter_lists()
+        self.generate_every_bar = generate_every_bar
+
+    def generate_parameter_lists(self):
+        if self.pitch_generator:
+            self.pitch_list = self.pitch_generator.generate_list()
+        else:
+            self.pitch_list = [60]*8
+        if self.duration_generator:
+            self.duration_list = self.duration_generator.generate_list()
+        else:
+            self.duration_list = [1]*8
+        if self.velocity_generator:
+            self.velocity_list = self.velocity_generator.generate_list()
+        else:
+            self.velocity_list = [100]*8
+
+        #find shortest list
+        min_length = min(len(self.pitch_list), len(self.duration_list), len(self.velocity_list))
+        #truncate lists
+        self.pitch_list = self.pitch_list[:min_length]
+        self.duration_list = self.duration_list[:min_length]
+        self.velocity_list = self.velocity_list[:min_length]
+
+        return
 
     def make_bar_list(self):
         self.bar_list = []
         onset = 0
         for i in range(self.num_bars):
-            bar = Bar(onset, self.ioi)
+            if self.generate_every_bar:
+                self.generate_parameter_lists()
+            bar = Bar(onset, self.ioi, self.pitch_list, self.duration_list, self.velocity_list)
             bar.make_note_list()
             self.bar_list.append(bar)
-            onset += bar.ioi*len(bar.pitch_list)
+            onset += bar.ioi*len(bar.note_list)
         return
     
     def make_midi_file(self, filename):
@@ -322,10 +296,46 @@ class Song:
                     note.onset = 0
         return
     
-song = Song(16)
-song.make_bar_list()
-song.modulate_onset_with_sin(3.6, 0.15)
-#song.modulate_duration_with_sin(2, 0.06)
-song.make_midi_file("test_grammars2.mid")
-print("Done")
+if __name__=="__main__":
+    pitch_grammar_str = """
+    S -> A 
+    A -> 60 A | 60 | 62 A | 62 | 60 B | 62 B
+    B -> 64 B | 64 | 65 B | 65 | 64 C | 65 C
+    C -> 67 C | 67 | 69 C | 69 | 67 D | 69 D
+    D -> 72 D | 72 | 71 D | 71 | 69 E | 71 E
+    E -> 67 E | 67 | 69 E | 69 | 67 F | 69 F
+    F -> 65 F | 65 | 64 F | 64 | 62 G | 64 G
+    G -> 60 G | 60 | 62 G | 62 | 60 H | 62 H
+    H -> 60 H | 60 | 62 H | 62 | 60 A | 62 A
+    """
 
+    duration_grammar_str = """
+    S -> A B C D E F G H
+    A -> 1 A | 0.5 A | 0.25 | 0.125
+    B -> 1 B | 0.5 B | 0.25 | 0.125
+    C -> 1 C | 0.5 C | 0.25 | 0.125
+    D -> 1 D | 0.5 D | 0.25 | 0.125
+    E -> 1 E | 0.5 E | 0.25 | 0.125
+    F -> 1 F | 0.5 F | 0.25 | 0.125
+    G -> 1 G | 0.5 G | 0.25 | 0.125
+    H -> 1 H | 0.5 H | 0.25 | 0.125
+    """
+
+    velocity_grammar_str = """
+    S -> A B C D E F G H
+    A -> 100 A | 90 A | 80 | 70
+    B -> 100 B | 90 B | 80 | 70
+    C -> 100 C | 90 C | 80 | 70
+    D -> 100 D | 90 D | 80 | 70
+    E -> 100 E | 90 E | 80 | 70
+    F -> 100 F | 90 F | 80 | 70
+    G -> 100 G | 90 G | 80 | 70
+    H -> 100 H | 90 H | 80 | 70
+    """
+
+    pitch_generator = ListGenerator(pitch_grammar_str, 8, "pitch")
+    duration_generator = ListGenerator(duration_grammar_str, 8, "duration")
+    velocity_generator = ListGenerator(velocity_grammar_str, 8, "velocity")
+    song = Song(16, pitch_generator, duration_generator, velocity_generator, True)
+    song.make_bar_list()
+    song.make_midi_file("testListGen.mid")
