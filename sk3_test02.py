20b576b77844021d76ce1f4ef07dcadbf1efd970
Jaakko Prättälä <iipekaj@gmail.com>
2023-10-13 11:58:41 +0300

    new test file

---
+++ sk3_test02.py
@@ -0,0 +1,35 @@
+from savellysKone3 import *
+
+pitch_grammar_string = """
+S -> a b b a
+a -> 60
+b -> 62 b 62 | 64 b 64 | 48 b 48 | 50 b 50 |
+b -> 36
+"""
+
+duration_grammar_string = """
+S -> a b b a
+a -> 0.6
+b -> 0.7 b 0.7 | 0.5 b 0.5 | 0.6 b 0.6
+b -> 0.25
+"""
+
+velocity_grammar_string = """
+S -> a b b a
+a -> 100
+b -> 100 b 100 | 80 b 80 | 60 b 60
+b -> 40
+"""
+
+#generators
+
+pitch_generator = ListGenerator(pitch_grammar_string, 8, "pitch")
+duration_generator = ListGenerator(duration_grammar_string, 8, "duration")
+velocity_generator = ListGenerator(velocity_grammar_string, 8, "velocity")
+
+#song
+song = Song(num_bars=8, ioi=0.5, pitch_generator=pitch_generator, duration_generator=duration_generator, velocity_generator=velocity_generator, generate_every_bar=True)
+
+song.make_bar_list()
+
+song.make_midi_file("sk3_test02.mid")
